e6019cf102e00535507154bee47a49e9d3006d71
lusine-muradyan <muradyanlusine@mail.ru>
2021-04-26 17:25:58 +0400

    Triangle

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a, b, c, area, perimeter;
+	a=3;
+	b=4;
+	c=5;
+	perimeter = a+b+c;
+	area = (a * b)/2;
+	System.out.println(perimeter);
+	System.out.println(area);
+    }
+}
